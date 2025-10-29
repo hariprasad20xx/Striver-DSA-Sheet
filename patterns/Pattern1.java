ee400acf0a06f7b6a9feb383525d43bab192b03c
Hariprasad <hariprasadjnv963@gmail.com>
2025-10-29 21:16:09 +0530

    Add logic to print rectangular pattern

--- patterns/Pattern1.java
+++ patterns/Pattern1.java
@@ -7,6 +7,15 @@ public class Pattern1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the value of N: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
         
     }
 }
