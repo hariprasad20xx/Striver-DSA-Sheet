c88273933d137ceb274a2b4915943ad1c87e0b6c
Hariprasad <hariprasadjnv963@gmail.com>
2025-10-29 21:06:36 +0530

    Create Scanner object

--- patterns/Pattern1.java
+++ patterns/Pattern1.java
@@ -1,9 +1,12 @@
 package patterns;
 
+import java.util.Scanner;
 
 public class Pattern1 {
     // Program to print Rectangular Star Pattern
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        
     }
 }
