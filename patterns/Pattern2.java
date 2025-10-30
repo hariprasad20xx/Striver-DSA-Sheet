1659475192f4898f7b5438f35a630ced5b7314a0
Hariprasad <hariprasadjnv963@gmail.com>
2025-10-30 16:48:15 +0530

    Add logic to print right angled triangle pattern

--- patterns/Pattern2.java
+++ patterns/Pattern2.java
@@ -7,6 +7,16 @@ public class Pattern2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+        System.out.print("Enter the value of N: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
         scanner.close();
     }
 }
