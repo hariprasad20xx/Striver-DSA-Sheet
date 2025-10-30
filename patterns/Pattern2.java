f06b617649ccf6e5d7225208ed9a5080776759e6
Hariprasad <hariprasadjnv963@gmail.com>
2025-10-30 16:41:49 +0530

    Create Scanner object

--- patterns/Pattern2.java
+++ patterns/Pattern2.java
@@ -1,8 +1,12 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class Pattern2 {
     // Program to print Right-Angled Triangle Pattern
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
+        scanner.close();
     }
 }
