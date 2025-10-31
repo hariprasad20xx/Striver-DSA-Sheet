b4f49d7d2dcd48b1646763417b6f5c08b0f8222d
Hariprasad <hariprasadjnv963@gmail.com>
2025-10-31 20:22:42 +0530

    Add logic to print star pyramid pattern

--- patterns/Pattern4.java
+++ patterns/Pattern4.java
@@ -1,8 +1,30 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class Pattern4 {
     // Program to print Star Pyramid pattern
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of N: ");
+        int n = scanner.nextInt();
+
+        // print star pyramid
+        for (int i = 1; i <= n; i++) {
+            // print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // print stars
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            
+            System.out.println();
+        }
+
+        scanner.close();
     }
 }
