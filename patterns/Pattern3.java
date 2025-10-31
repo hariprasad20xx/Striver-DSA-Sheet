f74c432459dda27fd80a9b3db5ba8b8fc221209a
Hariprasad <hariprasadjnv963@gmail.com>
2025-10-31 19:48:00 +0530

    Add logic to print inverted right angled triangle pattern

--- patterns/Pattern3.java
+++ patterns/Pattern3.java
@@ -7,6 +7,16 @@ public class Pattern3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the value of N: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
         scanner.close();
     }
 }
