2b515f3d1b0f77d26b49cd21a9f09ce2ba0dd160
Hariprasad <hariprasadjnv963@gmail.com>
2025-10-31 19:41:50 +0530

    Create Scanner object

--- patterns/Pattern3.java
+++ patterns/Pattern3.java
@@ -1,8 +1,12 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class Pattern3 {
     // Program to print Inverted Right-Angled Triangle Pattern
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
