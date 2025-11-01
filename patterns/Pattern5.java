e8229082eaa41e35a8c25150da2206d93adc42fb
Hariprasad <136832107+hariprasad20xx@users.noreply.github.com>
2025-11-01 11:53:27 +0530

    Create Scanner object

--- patterns/Pattern5.java
+++ patterns/Pattern5.java
@@ -1,7 +1,11 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class Pattern5 {
     public static void main(String[] args) {
-    
+        Scanner scannner = new Scanner(System.in);
+
+    scanner.close();
     }
 }
