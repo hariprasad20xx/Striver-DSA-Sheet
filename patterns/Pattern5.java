5dca6b718402bd260d52470cb7a5a1896d28400d
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-02 20:13:34 +0530

    Create method printSpaces

--- patterns/Pattern5.java
+++ patterns/Pattern5.java
@@ -3,6 +3,14 @@ package patterns;
 import java.util.Scanner;
 
 public class Pattern5 {
+    // Program to print Inverted Star Pyramid
+
+    public static void printSpaces(int n) {
+        for (int i = 1; i <= n; i++) {
+                System.out.print(" ");
+            }
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
