b2615bf95080f44554ea60d8e81fee0bcbddffd8
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-02 20:11:47 +0530

    Create method printStars

--- patterns/Pattern4.java
+++ patterns/Pattern4.java
@@ -11,6 +11,12 @@ public class Pattern4 {
             }
     }
 
+    public static void printStars(int n) {
+        for (int i = 1; i <= n; i++) {
+                System.out.print("*");
+            }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -21,12 +27,7 @@ public class Pattern4 {
         for (int i = 1; i <= n; i++) {
             
             printSpaces(n - i);
-            
-
-            // print stars
-            for (int j = 1; j <= 2 * i - 1; j++) {
-                System.out.print("*");
-            }
+            printStars(2 * i - 1);
             
             System.out.println();
         }
