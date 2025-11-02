cc05ccde725f1e8df163ffa82d11c5b79e226e45
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-02 20:14:25 +0530

    Create method printStars

--- patterns/Pattern5.java
+++ patterns/Pattern5.java
@@ -10,7 +10,14 @@ public class Pattern5 {
                 System.out.print(" ");
             }
     }
-    
+
+    public static void printStars(int n) {
+        for (int i = 1; i <= n; i++) {
+                System.out.print("*");
+            }
+    }
+
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
