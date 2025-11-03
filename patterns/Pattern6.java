42e98c3aa877444e9ff377c935ede258ea0103d3
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-03 21:20:02 +0530

    Create method printSpaces

--- patterns/Pattern6.java
+++ patterns/Pattern6.java
@@ -5,8 +5,18 @@ import java.util.Scanner;
 public class Pattern6 {
     // Program to print Diamond Star Pattern
 
+    public static void printSpaces(int n) {
+        for (int i = 1; i <= n; i++) {
+                System.out.print(" ");
+            }
+    }
+    
     public static void main(String[] agrs) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of N: ");
+        int n = scanner.nextInt();
+
 
         scanner.close();
     }
