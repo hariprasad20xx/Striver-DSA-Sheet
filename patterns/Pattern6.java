ba4e1e7936b8978b29a96db8e6e9856089a3acb9
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-03 21:16:27 +0530

    Create Scanner object

--- patterns/Pattern6.java
+++ patterns/Pattern6.java
@@ -1,9 +1,13 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class Pattern6 {
     // Program to print Diamond Star Pattern
 
     public static void main(String[] agrs) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
