44a50db7b3713d7022a9b4d2a00aa5132f105587
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-04 20:59:38 +0530

    Create Scanner object

--- patterns/Pattern7.java
+++ patterns/Pattern7.java
@@ -1,9 +1,13 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class Pattern7 {
     // Program to print Half Diamond Star Pattern
 
     public static void main(String[] agrs) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
