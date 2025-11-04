48fbe00edf015256564513795e6592201dd7d8f6
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-04 21:07:51 +0530

    Create method printSpaces

--- patterns/Pattern7.java
+++ patterns/Pattern7.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Pattern7 {
     // Program to print Half Diamond Star Pattern
 
+    public static void printStars(int n) {
+        for (int i = 1; i <= n; i++) {
+                System.out.print("*");
+            }
+    }
+    
     public static void main(String[] agrs) {
         Scanner scanner = new Scanner(System.in);
 
