5c60cad68245f5e1f8f542bcd829f49c9ee18df3
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-06 22:09:04 +0530

    Take user input

--- basic-maths/CountDigits.java
+++ basic-maths/CountDigits.java
@@ -4,6 +4,10 @@ public class CountDigits {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter an integer: ");
+        int n = scanner.nextInt();
+
+        
         scanner.close();
     }
 }
