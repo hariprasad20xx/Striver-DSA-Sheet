35a254dc00512c1fe6a5daef644ea1be085c1318
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-07 22:21:17 +0530

    Take user input

--- basic-maths/ReverseNumber.java
+++ basic-maths/ReverseNumber.java
@@ -5,6 +5,9 @@ public class ReverseNumber {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int n = scanner.nextInt();
 
         scanner.close();
     }
