e079d12068d83972141ba083ecebec752f8f810c
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-07 22:00:00 +0530

    Add main method

--- basic-maths/ReverseNumber.java
+++ basic-maths/ReverseNumber.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public class ReverseNumber {
-    
+    // Program to reverse a number    
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
 }
