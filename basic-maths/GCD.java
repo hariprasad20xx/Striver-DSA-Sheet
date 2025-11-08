9c04777fcda10ae6047f99b06c6532710528549a
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-08 21:44:35 +0530

    Add main method

--- basic-maths/GCD.java
+++ basic-maths/GCD.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+
 public class GCD {
-    
+    // Program to find GCD (or HCF) of the given two integers 
+
+    public static void main(String[] args) {
+        
+    }
 }
