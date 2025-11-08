a313ea3252dca85f0ae27b6e6b7c05b1dd6715a9
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-08 21:37:06 +0530

    Complete Palindrome.java

--- basic-maths/Palindrome.java
+++ basic-maths/Palindrome.java
@@ -4,6 +4,24 @@ public class Palindrome {
     // Program to check if a given integer is Palindrome or not
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int n = scanner.nextInt();
+
+        int copyOfN = n;
+        int rev = 0;
+        while (copyOfN != 0) {
+            rev = rev * 10 + (copyOfN % 10);
+            copyOfN /= 10;
+        } 
+
+        if (rev == n) {
+            System.out.println(n + " is a palindrome.");
+        } else {
+            System.out.println(n + " is not a palindrome.");
+        }
+
+        scanner.close();
     }
 }
