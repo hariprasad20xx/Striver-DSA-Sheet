9a2a01d02864687a11812897227230310e04d24a
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-08 08:39:31 +0530

    Complete Pattern7.java

--- patterns/Pattern7.java
+++ patterns/Pattern7.java
@@ -14,6 +14,20 @@ public class Pattern7 {
     public static void main(String[] agrs) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the value on N: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= 2 * n - 1; i++) {
+            
+            int numberOfStars = i;
+
+            if (i > n) numberOfStars = 2 * n - i;
+
+            printStars(numberOfStars);
+
+            System.out.println();
+        }
+
         scanner.close();
     }
 }
