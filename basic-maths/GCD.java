2cc7e04fb0b5c7667001b7446c02fbf28223db41
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-08 21:45:16 +0530

    Create Scanner object

--- basic-maths/GCD.java
+++ basic-maths/GCD.java
@@ -4,6 +4,8 @@ public class GCD {
     // Program to find GCD (or HCF) of the given two integers 
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
