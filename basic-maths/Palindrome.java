1be0711796500cf673234accf3e205ca857aff33
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-08 21:29:02 +0530

    Add main method

--- basic-maths/Palindrome.java
+++ basic-maths/Palindrome.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+
 public class Palindrome {
-    
+    // Program to check if a given integer is Palindrome or not
+
+    public static void main(String[] args) {
+        
+    }
 }
