583f34c4cf0d86fb2b8d23ce873f9b9e67fdc7b7
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-10 19:46:12 +0530

    Complete Armstrong.java

--- basic-maths/Armstrong.java
+++ basic-maths/Armstrong.java
@@ -1,7 +1,30 @@
 import java.util.Scanner;
 
 public class Armstrong {
+    // Program to check for Armstrong Number
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter an integer: ");
+        int n = scanner.nextInt();
+
+        int numberOfDigits = Integer.toString(n).length();
+
+        int temp = n;
+        int armstrong = 0;
+
+        while (temp != 0) {
+            armstrong += Math.pow(temp % 10, numberOfDigits);
+            temp /= 10;
+        }
+
+        if (armstrong == n) {
+            System.out.println(n + " is an armstrong number.");
+        } else {
+            System.out.println(n + " is not an armstrong number.");
+        }
+
+        scanner.close();
     }
 }
