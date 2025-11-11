d19d54d85c424dc4cbf9e18349beb95abe9530c0
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-11 21:24:52 +0530

    Add main method

--- basic-maths/Divisors.java
+++ basic-maths/Divisors.java
@@ -1,3 +1,9 @@
+import java.util.Scanner;
+
 public class Divisors {
-    
+    // Program to print all the divisors of an integer
+
+    public static void main(String[] args) {
+        
+    }
 }
