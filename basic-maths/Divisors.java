348839659f120402ac16ff297f1b838e961caec1
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-11 21:52:00 +0530

    Add logic to find all the divisors

--- basic-maths/Divisors.java
+++ basic-maths/Divisors.java
@@ -10,6 +10,17 @@ public class Divisors {
         System.out.println("Enter an integer: ");
         int n = scanner.nextInt();
 
+        ArrayList<Integer> divisors = new ArrayList<>();
+        divisors.add(1);
+
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0) {
+                divisors.add(i);
+            }
+        }
+        divisors.add(n);
+        
+
         scanner.close();
     }
 }
