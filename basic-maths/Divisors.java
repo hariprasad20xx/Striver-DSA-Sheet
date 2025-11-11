504363accc815def7ead725f6f558fd213006650
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-11 21:25:59 +0530

    Create Scanner object

--- basic-maths/Divisors.java
+++ basic-maths/Divisors.java
@@ -4,6 +4,8 @@ public class Divisors {
     // Program to print all the divisors of an integer
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
+        scanner.close();
     }
 }
