015c27e953884d931f733f03979205df55b83543
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-12 10:57:16 +0530

    Complete method isPrime

--- basic-maths/Prime.java
+++ basic-maths/Prime.java
@@ -5,7 +5,12 @@ public class Prime {
 
     public static boolean isPrime(int n) {
         
-        
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+
+            if (n % i == 0) {
+                return false;
+            }
+        }
 
         return true;
     }
