28b34f5826497e5e9bc3cc547392dcb1e4cf16e8
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-12 10:43:42 +0530

    Add main method

--- basic-maths/Prime.java
+++ basic-maths/Prime.java
@@ -1,3 +1,11 @@
+import java.util.Scanner;
+
 public class Prime {
-    
+    // Program to check if a given number is prime or not
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        scanner.close();
+    }
 }
