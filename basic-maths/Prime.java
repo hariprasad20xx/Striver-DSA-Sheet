81d98fccd09f4a47333f9b439f1b70205a240c97
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-12 10:48:08 +0530

    Complete main method

--- basic-maths/Prime.java
+++ basic-maths/Prime.java
@@ -6,6 +6,15 @@ public class Prime {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
+        System.out.print("Enter an integer: ");
+        int n = scanner.nextInt();
+
+        if (isPrime(n)) {
+            System.out.println(n + " is a prime number.");
+        } else {
+            System.out.println(n + " is not a prime number.");
+        }
+
         scanner.close();
     }
 }
