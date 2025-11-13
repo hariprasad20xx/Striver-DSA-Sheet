9c826200edba5dd2d7ea797aee7d6e7bac92df4a
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-13 17:36:10 +0530

    Add main method

--- patterns/Pattern8.java
+++ patterns/Pattern8.java
@@ -1,5 +1,14 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class Pattern8 {
-    
+    // Program to print Binary Triagle pattern
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        
+        scanner.close();
+    }
 }
