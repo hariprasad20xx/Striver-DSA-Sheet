16106a71416fb6eee3605eecc496e224c9d7b90d
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-13 18:08:47 +0530

    Add logic to print the pattern

--- patterns/Pattern9.java
+++ patterns/Pattern9.java
@@ -7,7 +7,30 @@ public class Pattern9 {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+        System.out.print("Enter the value of N: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            // print numbers in ascending order
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            // print spaces
+            for (int j = 1; j <= 2 * n - 2 * i; j++) {
+                System.out.print(" ");
+            }
+
+            // print numbers in descending order
+            for (int j = i; j >= 1; j--) {
+                System.out.print(j);
+            }
+            
+            System.out.println();
+        }
+
         scanner.close();
     }
 }
