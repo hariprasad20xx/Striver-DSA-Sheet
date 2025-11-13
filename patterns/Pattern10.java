71f6eb5254ea5a408f7799eccb78bdb67e70f6e3
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-13 20:14:17 +0530

    Add main method

--- patterns/Pattern10.java
+++ patterns/Pattern10.java
@@ -1,5 +1,17 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class Pattern10 {
-    
+    // Program to print Increasing Number Triangle Pattern
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of N: ");
+        int n = scanner.nextInt();
+
+        
+        scanner.close();
+    }
 }
