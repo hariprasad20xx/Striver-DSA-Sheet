b3a9f0ab727700949712f3a3138ade28d2d6b24d
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-13 18:01:28 +0530

    Add main method

--- patterns/Pattern9.java
+++ patterns/Pattern9.java
@@ -1,5 +1,13 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class Pattern9 {
-    
+    // Program to print Number Crown Pattern
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        scanner.close();
+    }
 }
