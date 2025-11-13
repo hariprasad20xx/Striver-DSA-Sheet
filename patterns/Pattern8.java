940f61dc6c8cb2ff3b2657009122c5fd6b391b2a
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-13 17:41:30 +0530

    Add logic to print the pattern

--- patterns/Pattern8.java
+++ patterns/Pattern8.java
@@ -10,7 +10,21 @@ public class Pattern8 {
 
         System.out.print("Enter the value of N: ");
         int n = scanner.nextInt();
+        
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
                 
+                if ((i + j) % 2 == 0) {
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+
+            System.out.println();
+        }
+
         scanner.close();
     }
 }
