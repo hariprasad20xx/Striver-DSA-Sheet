ce3da7c7be3a7a862478444164f949904a119271
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-14 22:26:13 +0530

    Add logic to print the pattern

--- patterns/Pattern13.java
+++ patterns/Pattern13.java
@@ -1,5 +1,27 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class Pattern13 {
-    
+    // Program to print Alpha-Ramp Pattern
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of N: ");
+        int n = scanner.nextInt();
+
+        char ch = 65;
+        for (int i = 1; i <= n; i++) {
+            
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch + " ");
+            }
+
+            ch++;
+            System.out.println();
+        }
+        
+        scanner.close();
+    }
 }
