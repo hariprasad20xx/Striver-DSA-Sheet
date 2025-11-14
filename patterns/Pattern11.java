d8ebc25c55104acb55610deb90654056c802a42a
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-14 22:12:57 +0530

    Add logic to print the pattern

--- patterns/Pattern11.java
+++ patterns/Pattern11.java
@@ -10,7 +10,16 @@ public class Pattern11 {
 
         System.out.print("Enter the value of N: ");
         int n = scanner.nextInt();
-        
+
+        for (int i = 1; i <= n; i++) {
+            char ch = 65;
+            
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch++ + " ");
+            }
+
+            System.out.println();
+        }
         scanner.close();
     }
 }
