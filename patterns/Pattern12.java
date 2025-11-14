2e3a8f701e0b4c3576cabc43f6b2c2979a988011
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-14 22:19:22 +0530

    Add logic to print the pattern

--- patterns/Pattern12.java
+++ patterns/Pattern12.java
@@ -1,5 +1,26 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class Pattern12 {
-    
+    // Program to print Reverse Letter Triangle Pattern
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of N: ");
+        int n = scanner.nextInt();
+
+        for (int i = n; i >= 1; i--) {
+            char ch = 65;
+            
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch++ + " ");
+            }
+
+            System.out.println();
+        }
+        
+        scanner.close();
+    }
 }
