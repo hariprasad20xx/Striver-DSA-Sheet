c303a6bd2a8290f8dc03f930deb2151e865e5361
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-15 19:44:38 +0530

    Add main method

---
+++ patterns/Pattern14.java
@@ -0,0 +1,17 @@
+package patterns;
+
+import java.util.Scanner;
+
+public class Pattern14 {
+    // Program to print Alpha-Hill Pattern
+ 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of N: ");
+        int n = scanner.nextInt();
+
+        
+        scanner.close();
+    }
+}
