42ea57994630431763eed49c8db069a52c0579e4
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-15 21:59:35 +0530

    Add logic to print the pattern

--- patterns/Pattern14.java
+++ patterns/Pattern14.java
@@ -11,7 +11,27 @@ public class Pattern14 {
         System.out.print("Enter the value of N: ");
         int n = scanner.nextInt();
 
-        
+        for (int i = 1; i <= n; i++) {
+
+            char ch = 65;
+            // print spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // print alphabets in ascending order
+            for (int j = 1; j <= i; j++) {
+                System.out.print(ch++);
+            }
+
+            // print alphabets in descending order
+            for (int j = 2; j <= i; j++) {   
+                System.out.print((char) (ch - j));
+            }
+
+            System.out.println();
+        }
+
         scanner.close();
     }
 }
