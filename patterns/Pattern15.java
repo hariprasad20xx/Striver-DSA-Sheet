dd326c69da6082eb0ae1e0507515ffc8086da0c6
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-16 22:01:24 +0530

    Handle user input

--- patterns/Pattern15.java
+++ patterns/Pattern15.java
@@ -8,6 +8,9 @@ public class Pattern15 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the value of N: ");
+        int n = scanner.nextInt();
+        
         scanner.close();
     }
 }
