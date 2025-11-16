9019ab282c759a4160c7fe442f038a20d832f09b
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-16 21:45:48 +0530

    Create Scanner object

--- patterns/Pattern15.java
+++ patterns/Pattern15.java
@@ -1,9 +1,13 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class Pattern15 {
     // Program to print Alpha-Triangle Pattern
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
