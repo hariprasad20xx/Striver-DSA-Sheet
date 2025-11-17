383ef74277394005ac000cec1f76bc9e41a3fef7
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-17 22:14:26 +0530

    Create Scanner object

--- patterns/Pattern16.java
+++ patterns/Pattern16.java
@@ -1,9 +1,13 @@
 package patterns;
 
+import java.util.Scanner;
+
 public class Pattern16 {
     // Program to print Symmetric-Void Pattern
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
