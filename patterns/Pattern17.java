b63c38b479872be40561f963986ab71bba3ac0a0
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-19 21:20:52 +0530

    Create Scanner object

--- patterns/Pattern17.java
+++ patterns/Pattern17.java
@@ -6,6 +6,8 @@ public class Pattern17 {
     // Program to print Symmetric-Butterfly Pattern
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
