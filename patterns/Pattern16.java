ce170e09e4017706bf4d7f76f5bff59f8e9d0760
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-25 19:59:36 +0530

    Implement pattern printing logic

--- patterns/Pattern16.java
+++ patterns/Pattern16.java
@@ -24,7 +24,25 @@ public class Pattern16 {
 
         System.out.print("Enter the value of N: ");
         int n = scanner.nextInt();
-        
+
+        int stars = 0;
+        int spaces = 0;
+
+        for (int i = 1; i <= 2 * n; i++) {
+            stars = n - i + 1;
+            spaces = 2 * i - 2;
+
+            if (i > n) {
+                stars = i - n;
+                spaces = (2 * n) - 2 * (i - n);
+            }
+
+            printStars(stars);
+            printSpaces(spaces);
+            printStars(stars);
+
+            System.out.println();
+        }
         scanner.close();
     }
 }
