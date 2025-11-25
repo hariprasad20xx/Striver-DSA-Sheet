852da52dd0133e4a3bb80251a303008a87b4eaf4
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-25 19:41:36 +0530

    Create method printStars

--- patterns/Pattern16.java
+++ patterns/Pattern16.java
@@ -5,8 +5,16 @@ import java.util.Scanner;
 public class Pattern16 {
     // Program to print Symmetric-Void Pattern
 
+    public static void printStars(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("*");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
 
         scanner.close();
     }
