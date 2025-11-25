6ff8238123bc0a0120293123c88af81366aa0afb
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-25 20:01:50 +0530

    Create method printStars

--- patterns/Pattern17.java
+++ patterns/Pattern17.java
@@ -5,6 +5,13 @@ import java.util.Scanner;
 public class Pattern17 {
     // Program to print Symmetric-Butterfly Pattern
 
+    public static void printStars(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("*");
+        }
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
