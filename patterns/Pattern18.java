404720b7b96fc742c38c330ff4449920ecc5b906
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-26 07:52:55 +0530

    Create method printStars

--- patterns/Pattern18.java
+++ patterns/Pattern18.java
@@ -5,9 +5,16 @@ import java.util.Scanner;
 public class Pattern18 {
     // Program to print Hollow Rectangle Pattern
 
+    public static void printStars(int n) {
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print("*");
+        }
+    }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
         scanner.close();
     }
 }
