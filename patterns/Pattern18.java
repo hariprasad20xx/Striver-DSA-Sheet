ea41e67b542ad8c15fc42cf8936f6ffe2b394ef6
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-26 08:07:39 +0530

    Implement pattern printing logic

--- patterns/Pattern18.java
+++ patterns/Pattern18.java
@@ -18,9 +18,25 @@ public class Pattern18 {
             System.out.print(" ");
         }
     }
-    
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of N: ");
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+
+            if (i == 1 || i == n) {
+                printStars(n);
+            } else {
+                printStars(1);
+                printSpaces(n - 2);
+                printStars(1);
+            }
+
+            System.out.println();  
+        }
 
         scanner.close();
     }
