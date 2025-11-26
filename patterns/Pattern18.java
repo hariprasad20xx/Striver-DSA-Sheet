f86158a8d507f22ca5fc77f33a5152a908488413
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-26 07:52:08 +0530

    Add main method

--- patterns/Pattern18.java
+++ patterns/Pattern18.java
@@ -1,5 +1,13 @@
 package patterns;
 
-public class Pattern18 {
+import java.util.Scanner;
 
+public class Pattern18 {
+    // Program to print Hollow Rectangle Pattern
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        scanner.close();
+    }
 }
