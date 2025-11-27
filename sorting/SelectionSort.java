0b4f299e55209360c66a00bf0a15ef0056ad5e9f
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-27 22:03:45 +0530

    Add main method

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -1,5 +1,12 @@
 package sorting;
 
+import java.util.Scanner;
+
 public class SelectionSort {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
 }
