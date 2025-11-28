64e585fd4025a0c36afd2f2b52864a5d8812925f
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-28 09:29:51 +0530

    Add main method

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -1,5 +1,12 @@
 package sorting;
 
+import java.util.Scanner;
+
 public class BubbleSort {
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
 }
