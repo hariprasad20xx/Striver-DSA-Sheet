8fba53cea71468c2243a4e478ffd05fc88b08c4f
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-28 10:32:58 +0530

    Implement sorting algorithm

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -7,6 +7,43 @@ public class BubbleSort {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter " + n + " elements for the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        for (int i = n - 1; i > 0; i--) {
+            boolean isSorted = true;
+
+            for (int j = 0; j < i; j++) {
+
+                if (arr[j] > arr[j + 1]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+
+                    isSorted = false;
+                }
+            }
+
+            if (isSorted) {
+                break;
+            }
+        }
+
+        System.out.print("Sorted Array: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
         scanner.close();
     }
 }
