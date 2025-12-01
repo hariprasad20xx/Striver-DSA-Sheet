9d345623a72535c0df2ffa5739091c6f9d84f701
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-01 18:37:10 +0530

    Implement sorting algorithm

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -16,7 +16,33 @@ public class InsertionSort {
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
-        
+
+        for (int i = 1; i < n; i++) {
+            int current = i;
+
+            for (int j =  i - 1; j >= 0; j--) {
+                
+                if (arr[j] > arr[current]) {
+
+                    int temp = arr[j];
+                    arr[j] = arr[current];
+                    arr[current] = temp;
+
+                    current = j;  
+                } else {
+                    break;
+                }
+                
+            }   
+        }
+
+        System.out.print("Sorted Array: ");
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
         scanner.close();
     }
 }
