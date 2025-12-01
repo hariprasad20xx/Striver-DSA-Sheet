004f8dff49cfb588bb15d80a14fdb0fdb813999e
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-01 18:18:12 +0530

    Handle user input

--- sorting/InsertionSort.java
+++ sorting/InsertionSort.java
@@ -7,6 +7,16 @@ public class InsertionSort {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter " + n + " elements for the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        
         scanner.close();
     }
 }
