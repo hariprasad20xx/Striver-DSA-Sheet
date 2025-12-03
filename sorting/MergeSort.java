5cbf6fef706d90110eb58e45f631a5dfdad73685
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-03 22:14:08 +0530

    Create Scanner object

--- sorting/MergeSort.java
+++ sorting/MergeSort.java
@@ -5,7 +5,9 @@ import java.util.Scanner;
 public class MergeSort {
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
     
 }
