224e9289db825d36150f042740f8a951305c2101
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-04 22:07:16 +0530

    Add main method

--- recursion/Recursion3.java
+++ recursion/Recursion3.java
@@ -1,5 +1,13 @@
 package recursion;
 
-public class Recursion3 {
+import java.util.Scanner;
 
+public class Recursion3 {
+    // Program to print 1 to N using recursion
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
+    }
 }
