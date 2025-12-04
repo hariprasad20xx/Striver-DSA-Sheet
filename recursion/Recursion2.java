8232432c4b7711a49f0dd469a3d794cf503be765
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-04 21:59:24 +0530

    Complete Recursion2.java

--- recursion/Recursion2.java
+++ recursion/Recursion2.java
@@ -5,8 +5,21 @@ import java.util.Scanner;
 public class Recursion2 {
     // Program to print 1 to N using recursion
 
+    public static void printNumber(int n) {
+        if (n > 1) printNumber(n - 1);
+
+        System.out.print(n + " ");
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of N: ");
+        int n = scanner.nextInt();
+
+        printNumber(n);
+
+        System.out.println();
 
         scanner.close();
     }
