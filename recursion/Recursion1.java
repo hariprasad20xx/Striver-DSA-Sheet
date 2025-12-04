f02eb29adfbebcc8fbc4bd36ed6b0e647764428a
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-04 21:31:16 +0530

    Complete Recursion1.java

--- recursion/Recursion1.java
+++ recursion/Recursion1.java
@@ -3,10 +3,21 @@ package recursion;
 import java.util.Scanner;
 
 public class Recursion1 {
-    // Program to print a name N times
+    // Program to print my name N times
+
+    public static void printName(String name, int n) {
+        if (n > 1) printName(name, n - 1);
+
+        System.out.println(name);
+    }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the value of N: ");
+        int n = scanner.nextInt();
+
+        printName("Hariprasad", n);
 
         scanner.close();
     }
