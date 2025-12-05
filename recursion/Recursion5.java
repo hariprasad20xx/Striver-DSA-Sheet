7a2f8436949fe63a0a4752e0e5f4ebfa885bc717
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-05 18:42:49 +0530

    Implement recursive logic with base case

--- recursion/Recursion5.java
+++ recursion/Recursion5.java
@@ -6,8 +6,9 @@ public class Recursion5 {
     // Program to find Factorial of a given number
 
     public static int factorial(int n) {
-        return 0;
+        return (n == 0) ? 1 : n * factorial(n - 1);
     }
+    
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
