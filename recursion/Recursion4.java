bcb1d510e29f8ed822df2802691c633568540396
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-05 18:11:47 +0530

    Add method signature for recursive sum calculation

--- recursion/Recursion4.java
+++ recursion/Recursion4.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Recursion4 {
     // Program to calculate the sum of first N natural numbers using recursion
 
+    public static int sumOfNaturalNumbers(int n) {
+        return 0;
+    } 
     public static void main(String[] agrs) {
         Scanner scanner = new Scanner(System.in);
 
