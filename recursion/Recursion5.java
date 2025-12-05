766d0ca313306158f881a0770f08fe7475f84860
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-05 18:40:08 +0530

    Add method signature for recursive factorial method

--- recursion/Recursion5.java
+++ recursion/Recursion5.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Recursion5 {
     // Program to find Factorial of a given number
 
+    public static int factorial(int n) {
+        return 0;
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
