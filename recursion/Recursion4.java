29353846b61a96b895782fdce2cb9acfd96bb30d
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-05 18:13:53 +0530

    Implement recursive logic with base case

--- recursion/Recursion4.java
+++ recursion/Recursion4.java
@@ -6,8 +6,9 @@ public class Recursion4 {
     // Program to calculate the sum of first N natural numbers using recursion
 
     public static int sumOfNaturalNumbers(int n) {
-        return 0;
+        return (n == 1) ? 1 : n + sumOfNaturalNumbers(n - 1);
     } 
+    
     public static void main(String[] agrs) {
         Scanner scanner = new Scanner(System.in);
 
