158566918ca93bb71528a709c89d947ecb9c7059
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-07 22:12:14 +0530

    Create Scanner object

--- recursion/Recursion6.java
+++ recursion/Recursion6.java
@@ -1,9 +1,13 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class Recursion6 {
     // Program to reverse an array
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
