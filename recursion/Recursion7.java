3d9929aa16522c80c96165336e7d8f5087daeed4
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-07 22:15:37 +0530

    Create Scanner object

--- recursion/Recursion7.java
+++ recursion/Recursion7.java
@@ -1,9 +1,13 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class Recursion7 {
     // Program to check if a string is a palindrome
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
