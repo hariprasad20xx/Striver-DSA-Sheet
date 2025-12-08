6ca4ef79a2a8eeba9b75021fb343ea5948f029eb
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-08 21:40:27 +0530

    Create Scanner object

--- patterns/Pattern19.java
+++ patterns/Pattern19.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Pattern19 {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
