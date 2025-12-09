433f33dfe3a116009f5a3fde3cce13487be8c85b
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-09 18:46:07 +0530

    Add method signature for recursive method checkPalindrome

--- recursion/Recursion7.java
+++ recursion/Recursion7.java
@@ -5,6 +5,10 @@ import java.util.Scanner;
 public class Recursion7 {
     // Program to check if a string is a palindrome
 
+    public static boolean checkPalindrome(String s, int p1, int p2) {
+        return true;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
