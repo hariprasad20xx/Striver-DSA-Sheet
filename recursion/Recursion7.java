a7ea48ec478723eeba85d69c3629adbc9d31ac44
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-09 18:56:19 +0530

    Implement recursive logic to check for palindrome

--- recursion/Recursion7.java
+++ recursion/Recursion7.java
@@ -6,7 +6,10 @@ public class Recursion7 {
     // Program to check if a string is a palindrome
 
     public static boolean checkPalindrome(String s, int p1, int p2) {
-        return true;
+
+        if (s.charAt(p1) != s.charAt(p2)) return false;
+        
+        return (p1 >= p2) ? true : checkPalindrome(s, p1 + 1, p2 - 1);
     }
 
     public static void main(String[] args) {
