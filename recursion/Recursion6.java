adc3346de83028d429f79cccd9a3d94e1ad294a8
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-09 17:30:44 +0530

    Take user input

--- recursion/Recursion6.java
+++ recursion/Recursion6.java
@@ -8,6 +8,22 @@ public class Recursion6 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter " + n + " elements for the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.print("Original Array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.println(arr[i] + " ");
+        }
+
+        
         scanner.close();
     }
 }
