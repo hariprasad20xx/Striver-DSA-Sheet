34d1bd50e6f828c503c9beb917f7cb99dbb3e26b
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-10 19:46:46 +0530

    Take user input

--- patterns/Pattern19.java
+++ patterns/Pattern19.java
@@ -7,6 +7,11 @@ public class Pattern19 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the value of N: ");
+        int n = scanner.nextInt();
+
+        
+
         scanner.close();
     }
 }
