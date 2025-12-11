397a2d036a2310371dcd9d355b5d3401881df97e
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-11 21:45:33 +0530

    Update comment

--- recursion/Recursion8.java
+++ recursion/Recursion8.java
@@ -1,7 +1,7 @@
 package recursion;
 
 public class Recursion8 {
-    // Program to print the Fibonacci Sequence using Recursion
+    // Program to print Nth Fibonacci number using Recursion
 
     public static void main(String[] args) {
         
