71f3ae5ade0d8c3db4451c693fa3f409032048a5
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-11 21:46:42 +0530

    Create Scanner object

--- recursion/Recursion8.java
+++ recursion/Recursion8.java
@@ -1,9 +1,13 @@
 package recursion;
 
+import java.util.Scanner;
+
 public class Recursion8 {
     // Program to print Nth Fibonacci number using Recursion
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
