46acf796db4699c125ab5f346c21711b44e4cf1c
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-12 22:07:15 +0530

    Create Scanner object

--- arrays/LargestElement.java
+++ arrays/LargestElement.java
@@ -6,6 +6,8 @@ public class LargestElement {
     // Program to find largest element in an array
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
