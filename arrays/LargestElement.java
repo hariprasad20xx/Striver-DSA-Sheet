412339d7c54c53371ed17f034b5bc0ea5902700c
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-12 22:06:28 +0530

    Import Scanner class

--- arrays/LargestElement.java
+++ arrays/LargestElement.java
@@ -1,9 +1,11 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class LargestElement {
     // Program to find largest element in an array
 
     public static void main(String[] args) {
-        
+
     }
 }
