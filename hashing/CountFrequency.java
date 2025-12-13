58976a42466b19dbb16802806d9e7e10b65696e5
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-13 22:08:03 +0530

    Handle user input

--- hashing/CountFrequency.java
+++ hashing/CountFrequency.java
@@ -10,6 +10,16 @@ public class CountFrequency {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+
+        int[] array = new int[n];
+
+        System.out.print("Enter " + n + " elements for the array: ");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        
         scanner.close();
     }
 }
