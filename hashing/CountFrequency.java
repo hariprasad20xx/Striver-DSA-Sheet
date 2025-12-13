bd30e45bb948d4b56b8088ae195ecf5e5bf6ce39
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-13 22:03:50 +0530

    Import required classes

--- hashing/CountFrequency.java
+++ hashing/CountFrequency.java
@@ -1,5 +1,9 @@
 package hashing;
 
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountFrequency {
     // Program to count frequency of each element in the given array
     
