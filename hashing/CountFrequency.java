26d0cf0667f155b81d7f488ebd7577717fb06301
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-13 22:04:53 +0530

    Create Scanner object

--- hashing/CountFrequency.java
+++ hashing/CountFrequency.java
@@ -8,6 +8,8 @@ public class CountFrequency {
     // Program to count frequency of each element in the given array
     
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
