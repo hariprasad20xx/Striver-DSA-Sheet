0f02e02678c26d368f656a87c06667198f081a0d
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-14 22:51:08 +0530

    Import Scanner class

--- arrays/SecondLargest.java
+++ arrays/SecondLargest.java
@@ -1,9 +1,11 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class SecondLargest {
     // Program to find the second largest element in an array without sorting
 
     public static void main(String[] args) {
-        
+
     }
 }
