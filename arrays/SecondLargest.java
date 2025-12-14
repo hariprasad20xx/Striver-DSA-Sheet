6465e810327a9e82bd82f9379d941588f23f2861
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-14 22:50:29 +0530

    Add main method

--- arrays/SecondLargest.java
+++ arrays/SecondLargest.java
@@ -3,5 +3,7 @@ package arrays;
 public class SecondLargest {
     // Program to find the second largest element in an array without sorting
 
-    
+    public static void main(String[] args) {
+        
+    }
 }
