2325dcaafabf0ac5a4cef9afe95eff4ec76245ba
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-14 22:51:51 +0530

    Create Scanner object

--- arrays/SecondLargest.java
+++ arrays/SecondLargest.java
@@ -6,6 +6,8 @@ public class SecondLargest {
     // Program to find the second largest element in an array without sorting
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
