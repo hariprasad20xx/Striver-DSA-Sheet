1cd87ff3d22fc7dabc7bf8a7e595de8fcc32c029
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-15 22:13:26 +0530

    Create Scanner object

--- hashing/Frequency.java
+++ hashing/Frequency.java
@@ -6,6 +6,8 @@ public class Frequency {
     // Program to find the highest and lowest frequency element
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
