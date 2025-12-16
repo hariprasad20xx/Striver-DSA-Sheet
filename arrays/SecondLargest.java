366081a38aee9456acd4c29c56e484a31a4710b7
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-16 21:12:42 +0530

    Implement logic to find the second largest element

--- arrays/SecondLargest.java
+++ arrays/SecondLargest.java
@@ -18,6 +18,18 @@ public class SecondLargest {
             arr[i] = scanner.nextInt();
         }
 
+        int largest = arr[0];
+        int secondLargest = -1;
+
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] < largest) {
+                secondLargest = arr[i];
+            }
+        }
+
         scanner.close();
     }
 }
