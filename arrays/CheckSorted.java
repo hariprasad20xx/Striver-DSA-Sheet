55e4f0c7dd4ef37e3521bd52167910e59a6885d1
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-24 22:07:54 +0530

    Take user input

--- arrays/CheckSorted.java
+++ arrays/CheckSorted.java
@@ -8,6 +8,16 @@ public class CheckSorted {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter " + n + " elements for the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        
         scanner.close();
     }
 }
