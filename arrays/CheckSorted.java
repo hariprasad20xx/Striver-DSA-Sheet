186b65c2148f3cd2292c7178c078cadc560b5439
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-24 22:06:40 +0530

    Create Scanner object

--- arrays/CheckSorted.java
+++ arrays/CheckSorted.java
@@ -6,6 +6,8 @@ public class CheckSorted {
     // Program to check if the given array is sorted
 
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
