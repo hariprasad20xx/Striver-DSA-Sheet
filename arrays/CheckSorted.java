f855e6d7d40b15bfcbe9b830ff71f468c9da1b68
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-25 21:43:38 +0530

    Implement logic to check for sorted array

--- arrays/CheckSorted.java
+++ arrays/CheckSorted.java
@@ -17,7 +17,14 @@ public class CheckSorted {
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
-        
+
+        boolean isSorted = true;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                isSorted = false;
+            }
+        }
+
         scanner.close();
     }
 }
