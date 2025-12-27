8c0a6893ad94adee9209cd23c49f8b4e158aa971
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-27 22:19:15 +0530

    Handle user input

--- arrays/TwoSum.java
+++ arrays/TwoSum.java
@@ -7,6 +7,16 @@ public class TwoSum {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter " + n + " elements for the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        
         scanner.close();
     }
 }
