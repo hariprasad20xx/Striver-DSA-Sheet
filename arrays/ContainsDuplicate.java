f35ed639efad873f607889ff549604df44a0fd08
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-28 23:31:46 +0530

    Create Scanner object

--- arrays/ContainsDuplicate.java
+++ arrays/ContainsDuplicate.java
@@ -6,6 +6,8 @@ public class ContainsDuplicate {
     // Program to check whether the given array contains duplicate element
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
