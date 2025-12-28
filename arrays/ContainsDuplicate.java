1486bff8c15ee202479bef12d75358217e88acb5
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-28 23:29:37 +0530

    Add main method

--- arrays/ContainsDuplicate.java
+++ arrays/ContainsDuplicate.java
@@ -1,5 +1,9 @@
 package arrays;
 
 public class ContainsDuplicate {
+    // Program to check whether the given array contains duplicate element
 
+    public static void main(String[] args) {
+        
+    }
 }
