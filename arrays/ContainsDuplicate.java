2fbbdaa2d06724bfac2ad80d1ebcf284aba03236
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-28 23:30:35 +0530

    Import Scanner class

--- arrays/ContainsDuplicate.java
+++ arrays/ContainsDuplicate.java
@@ -1,9 +1,11 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class ContainsDuplicate {
     // Program to check whether the given array contains duplicate element
 
     public static void main(String[] args) {
-        
+
     }
 }
