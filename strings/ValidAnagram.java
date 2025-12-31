23d286919fcf48d040139dec4e752c6de6e50586
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-31 18:45:17 +0530

    Import Scanner class

--- strings/ValidAnagram.java
+++ strings/ValidAnagram.java
@@ -1,9 +1,11 @@
 package strings;
 
+import java.util.Scanner;
+
 public class ValidAnagram {
     // Program to check whether given two strings are anagrams of each other
 
     public static void main(String[] args) {
-        
+
     }
 }
