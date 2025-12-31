c122a7705ec5cf13c21c9b27de57487deb0c0bfe
Hariprasad <hariprasadjnv963@gmail.com>
2025-12-31 18:43:53 +0530

    Add main method

--- strings/ValidAnagram.java
+++ strings/ValidAnagram.java
@@ -2,4 +2,8 @@ package strings;
 
 public class ValidAnagram {
     // Program to check whether given two strings are anagrams of each other
+
+    public static void main(String[] args) {
+        
+    }
 }
