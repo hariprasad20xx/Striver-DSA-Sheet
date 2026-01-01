58f071831594045d2ea31602893a582cadf8a0e0
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-01 21:57:26 +0530

    Implement algorithm to check anagrams

--- strings/ValidAnagram.java
+++ strings/ValidAnagram.java
@@ -6,6 +6,25 @@ public class ValidAnagram {
     // Program to check whether given two strings are anagrams of each other
 
     public static boolean isAnagram(String s, String t) {
+
+        // if lengths are different, return false
+        if (s.length() != t.length()) return false;
+
+        // create array to store frequency of each lowercase letter
+        int[] count = new int[26];
+
+        // store the frequency of letters in s in the array
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
+        }
+
+        for (char c : t.toCharArray()) {
+            // if there is difference in the frequency, return false
+            if (count[c - 'a']-- == 0) {
+                return false;
+            }
+        }
+        
         return true;
     }
 
