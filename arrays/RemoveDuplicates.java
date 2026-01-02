d51493962e9463ff49af7f4505fa2ffa54ec329e
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-02 21:58:34 +0530

    Import Scanner class

--- arrays/RemoveDuplicates.java
+++ arrays/RemoveDuplicates.java
@@ -1,9 +1,11 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class RemoveDuplicates {
     // Program to remove duplicates in a sorted array (in-place)
 
     public static void main(String[] args) {
-        
+
     }
 }
