53349d93c76af746b6e8f80f9fb9175f2b7a8457
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-08 22:59:04 +0530

    Input array size

--- arrays/RemoveDuplicates.java
+++ arrays/RemoveDuplicates.java
@@ -8,6 +8,9 @@ public class RemoveDuplicates {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+        
         scanner.close();
     }
 }
