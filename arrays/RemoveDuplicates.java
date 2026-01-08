a539c237ca79fdbea61f3e57a84f93e7ff1e0376
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-08 23:00:55 +0530

    Input sorted array with duplicates

--- arrays/RemoveDuplicates.java
+++ arrays/RemoveDuplicates.java
@@ -13,6 +13,11 @@ public class RemoveDuplicates {
 
         int[] arr = new int[n];
 
+        System.out.print("Enter sorted array of size" + n + " with duplicates: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
         scanner.close();
     }
 }
