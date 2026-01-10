3c50842946c4bcfbdaf1bce780127448bd2245c3
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-10 22:20:36 +0530

    Implement algorithm to remove duplicates

--- arrays/RemoveDuplicates.java
+++ arrays/RemoveDuplicates.java
@@ -8,7 +8,20 @@ public class RemoveDuplicates {
     public static int removeDuplicates(int[] nums) {
         if (nums.length == 0) return 0;
         
+        int i = 0;
+
+        for (int j = 1; j < nums.length; j++) {
+
+            if (nums[j] != nums[i]) {
+               
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+
+        return i + 1;
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -21,8 +34,6 @@ public class RemoveDuplicates {
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
-
-
 
         scanner.close();
     }
