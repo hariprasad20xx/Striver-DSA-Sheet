04d03db94d0a4d1c10bb4a129efd6d66f0796a2c
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-10 22:26:18 +0530

    Output result to the console

--- arrays/RemoveDuplicates.java
+++ arrays/RemoveDuplicates.java
@@ -30,13 +30,20 @@ public class RemoveDuplicates {
 
         int[] arr = new int[n];
 
-        System.out.print("Enter sorted array of size" + n + " with duplicates: ");
+        System.out.print("Enter sorted array of size " + n + " with duplicates: ");
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
 
         int k = removeDuplicates(arr);
         
+        System.out.println("Unique count: " + k);
+        System.out.print("Array after removing duplicates: ");
+        for (int i = 0; i < k; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+
         scanner.close();
     }
 }
