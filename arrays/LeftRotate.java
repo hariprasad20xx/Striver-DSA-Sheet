2d39bdd4ea85f0e9f6fdf1477f4591c31379e455
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-14 20:58:11 +0530

    Create array of given size

--- arrays/LeftRotate.java
+++ arrays/LeftRotate.java
@@ -9,6 +9,8 @@ public class LeftRotate {
 
         System.out.print("Enter the size of the array: ");
         int n = scanner.nextInt();
+
+        int[] arr = new int[n];
         
         scanner.close();
     }
