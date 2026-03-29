5a27529a91ba47b0ddb22f7b96ed5e2e0eb23f7b
Hariprasad <hariprasadjnv963@gmail.com>
2026-03-29 22:11:10 +0530

    Import Scanner class

--- sorting/QuickSort.java
+++ sorting/QuickSort.java
@@ -1,9 +1,11 @@
 package sorting;
 
+import java.util.Scanner;
+
 public class QuickSort {
     // Program to sort an array using Quick Sort
 
     public static void main(String[] args) {
-        
+
     }
 }
