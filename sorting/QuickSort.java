6e3b07a868e10ccb25cf0a95d5d2e5cce7df57cc
Hariprasad <hariprasadjnv963@gmail.com>
2026-03-30 22:48:46 +0530

    Add user input for array elements

--- sorting/QuickSort.java
+++ sorting/QuickSort.java
@@ -13,6 +13,11 @@ public class QuickSort {
 
         int[] arr = new int[n];
 
+        System.out.print("Enter " + n + " elements for the array: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
         scanner.close();
     }
 }
