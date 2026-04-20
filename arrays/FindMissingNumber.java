870adfa92d578a46e3b16f378ecb04d32a5bbb9b
Hariprasad <hariprasadjnv963@gmail.com>
2026-04-20 21:47:32 +0530

    Add user input for array elements

--- arrays/FindMissingNumber.java
+++ arrays/FindMissingNumber.java
@@ -12,6 +12,11 @@ public class FindMissingNumber {
 
         int[] arr = new int[n];
 
+        System.out.print("Enter sorted array of size " + n + " with duplicates: ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
         scanner.close();
     }
 }
