84ca22937be3b59f5427f1f098017adf44d78fec
Hariprasad <hariprasadjnv963@gmail.com>
2026-04-21 22:17:45 +0530

    Add method signature for missingNumber method

--- arrays/FindMissingNumber.java
+++ arrays/FindMissingNumber.java
@@ -4,6 +4,10 @@ import java.util.Scanner;
 
 public class FindMissingNumber {
 
+    public static int missingNumber(int[] arr) {
+        return 0;
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
