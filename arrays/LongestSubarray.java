3a64fcc757c47e4fe03f2579eb0e2d06472cab35
Hariprasad <hariprasadjnv963@gmail.com>
2026-05-03 22:46:31 +0530

    Create array of given size

--- arrays/LongestSubarray.java
+++ arrays/LongestSubarray.java
@@ -10,6 +10,8 @@ public class LongestSubarray {
         System.out.print("Enter the size of the array: ");
         int n = scanner.nextInt();
 
+        int[] arr = new int[n];
+
         scanner.close();
     }
 }
