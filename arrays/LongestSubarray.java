f35d636dc219c37f28c66ea84d0890844a5151ae
Hariprasad <hariprasadjnv963@gmail.com>
2026-05-03 22:45:11 +0530

    Create Scanner object

--- arrays/LongestSubarray.java
+++ arrays/LongestSubarray.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class LongestSubarray {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
