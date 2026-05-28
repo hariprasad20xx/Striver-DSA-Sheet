8317a2ad3b19b40d6866fb1567e08f699f1c2509
Hariprasad <hariprasadjnv963@gmail.com>
2026-05-28 22:32:25 +0530

    Add logic to find longest subarray with sum zero

--- arrays/LongestZeroSumSubarray.java
+++ arrays/LongestZeroSumSubarray.java
@@ -2,6 +2,7 @@ package arrays;
 
 import java.util.Scanner;
 import java.util.HashMap;
+import java.util.Map;
 
 public class LongestZeroSumSubarray {
 
@@ -12,7 +13,23 @@ public class LongestZeroSumSubarray {
         int maxi = 0;
         int sum = 0;
 
-        return 0;
+        for (int i = 0; i < n; i++) {
+
+            sum += A[i];
+
+            if (sum == 0) {
+                maxi = i + 1;
+            } else {
+            
+                if (mpp.containsKey(sum)) {   
+                    maxi = Math.max(maxi, i - mpp.get(sum));
+                } else {
+                    mpp.put(sum, i);
+                }
+            }
+        }
+
+        return maxi;
     }
 
     public static void main(String[] args) {
