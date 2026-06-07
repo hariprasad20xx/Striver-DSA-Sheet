b5cb0a34fb413a9edbc653c5fa074aebd5f19ff7
Hariprasad <hariprasadjnv963@gmail.com>
2026-06-07 22:37:16 +0530

    Add logic to find maximum sum subarray

--- arrays/MaximumSumSubarray.java
+++ arrays/MaximumSumSubarray.java
@@ -5,7 +5,41 @@ import java.util.Scanner;
 public class MaximumSumSubarray {
 
     static int maxSubArray(int[] nums) {
+        
+        long maxi = Long.MIN_VALUE; 
+        
+        long sum = 0; 
+        
+        int start = 0; 
+        
+        int ansStart = -1, ansEnd = -1; 
+        
+        for (int i = 0; i < nums.length; i++) {
+            
+            if (sum == 0) {
+                start = i;
+            }
+            
+            sum += nums[i]; 
+            
+            if (sum > maxi) {
+                maxi = sum;
+                ansStart = start;
+                ansEnd = i;
+            }
+            
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        
+        System.out.print("The subarray is: [");
+        for (int i = ansStart; i <= ansEnd; i++) {
+            System.out.print(nums[i] + " ");
+        }
+        System.out.println("]");
 
+        return (int) maxi;
     }
     
     public static void main(String[] args) {
