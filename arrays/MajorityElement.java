79e6d8d9245d5b5dcda219b676431936e2e4d804
Hariprasad <hariprasadjnv963@gmail.com>
2026-06-09 21:46:36 +0530

    Create Scanner object

--- arrays/MajorityElement.java
+++ arrays/MajorityElement.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class MajorityElement {
 
     public static void main(String[] agrs) {
-
+        Scanner scanner = new Scanner(System.in);
+        
+        scanner.close();
     }
 }
