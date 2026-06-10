981c4f526cb6cc5ad7732c30cefa3b39437becf8
Hariprasad <hariprasadjnv963@gmail.com>
2026-06-10 21:23:27 +0530

    Add user input for array size

--- arrays/MajorityElement.java
+++ arrays/MajorityElement.java
@@ -6,6 +6,9 @@ public class MajorityElement {
 
     public static void main(String[] agrs) {
         Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
         
         scanner.close();
     }
