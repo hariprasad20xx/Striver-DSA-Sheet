77ba940a82fb5bbcb72b977b01176f4b12b2d70c
Hariprasad <hariprasadjnv963@gmail.com>
2026-06-11 21:15:21 +0530

    Add method call for majorityElement method in main

--- arrays/MajorityElement.java
+++ arrays/MajorityElement.java
@@ -48,6 +48,8 @@ public class MajorityElement {
         for (int i = 0; i < n; i++) {
             arr[i] = scanner.nextInt();
         }
+
+        int result = majorityElement(arr);
         
         scanner.close();
     }
