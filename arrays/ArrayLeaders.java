ad16addced6e4e25537c44e6f3912de476ca0fcc
Hariprasad <hariprasadjnv963@gmail.com>
2026-06-12 22:13:28 +0530

    Create Scanner object

--- arrays/ArrayLeaders.java
+++ arrays/ArrayLeaders.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class ArrayLeaders {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
