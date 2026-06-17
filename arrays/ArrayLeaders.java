8a66f782297163c91897d96a09a153920f4366fd
Hariprasad <hariprasadjnv963@gmail.com>
2026-06-17 17:32:45 +0530

    Implement logic to find array leaders

--- arrays/ArrayLeaders.java
+++ arrays/ArrayLeaders.java
@@ -2,11 +2,31 @@ package arrays;
 
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class ArrayLeaders {
 
     static ArrayList<Integer> leaders(int[] nums) {
         ArrayList<Integer> ans = new ArrayList<>();
+
+        if (nums.length == 0) {
+            return ans;
+        }
+        
+        // Last element of the array is always a leader
+        int max = nums[nums.length - 1];
+        ans.add(nums[nums.length - 1]);
+        
+        // Check elements from right to left
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (nums[i] > max) {
+                ans.add(nums[i]);
+                max = nums[i];
+            }
+        }
+        
+        // reverse the list to match required output
+        Collections.reverse(ans);
 
         return ans;
     }
